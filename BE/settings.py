93bdcc0dc2aa33524db74d62f23c26a487018f1b
honggyu1233@naver.com <honggyu1233@naver.com>
2024-08-05 14:08:47 +0900

    cors error

--- BE/settings.py
+++ BE/settings.py
@@ -128,15 +128,19 @@ ACCOUNT_LOGOUT_ON_GET = True
 
 
 
-
-
-
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
-    "http://localhost:5173",]
+    "http://localhost:5173",
+    "https://yellowtaxi.store",
+    "https://yellowtaxi.store/",
+    ]
+
+
+
+
 
 CORS_ALLOW_CREDENTIALS = True
 
